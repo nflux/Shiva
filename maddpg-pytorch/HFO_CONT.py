Softmax across action space with working inverted gradients. fixed bugs in HFO_CONT and move critic input to onehot encoding across action space

maddpg-pytorch/HFO_CONT.py
@@ -49,7 +49,7 @@ episode_length = 500 # FPS
 
 replay_memory_size = 1000000
 num_explore_episodes = 40  # Haus uses over 10,000 updates --
-burn_in_iterations = 500000 # for time step
+burn_in_iterations = 50000 # for time step
 burn_in_episodes = float(burn_in_iterations)/episode_length
 USE_CUDA = False 
 
@@ -154,25 +154,24 @@ for ep_i in range(0, num_episodes):
             # convert actions to numpy arrays
             agent_actions = [ac.data.numpy() for ac in torch_agent_actions]
             # rearrange actions to be per environment
+            params = np.asarray([ac[0][len(env.action_list):] for ac in agent_actions]) 
+
             
-            
-            actions_params_for_buffer = np.array([[ac[i] for ac in agent_actions] for i in range(1)]).reshape(
-                env.num_TA,env.action_params.shape[1] + len(env.action_list)) # concatenated actions, params for buffer
+           
             actions = [[ac[i][:len(env.action_list)] for ac in agent_actions] for i in range(1)] # this is returning one-hot-encoded action for each agent 
             if explore:
-                noisey_actions = [onehot_from_logits(torch.tensor(a).view(1,3),eps = (final_noise_scale + (init_noise_scale - final_noise_scale) * explr_pct_remaining)) for a in actions]     # get eps greedy action
+                noisey_actions = [onehot_from_logits(torch.tensor(a).view(1,len(env.action_list)),eps = (final_noise_scale + (init_noise_scale - final_noise_scale) * explr_pct_remaining)) for a in actions]     # get eps greedy action
             else:
-                noisey_actions = [onehot_from_logits(torch.tensor(a).view(1,3),eps = 0) for a in actions]     # get eps greedy action
+                noisey_actions = [onehot_from_logits(torch.tensor(a).view(1,len(env.action_list)),eps = 0) for a in actions]     # get eps greedy action
 
-
-            
-            params = np.asarray([ac[0][len(env.action_list):] for ac in agent_actions])
-            #print("ACTIONS: ",actions)
-            agents_actions = [np.argmax(agent_act_one_hot) for agent_act_one_hot in noisey_actions[0]] # convert the one hot encoded actions  to list indexes 
-
-     
+            noisey_actions_for_buffer = [ac.data.numpy() for ac in noisey_actions]
+            noisey_actions_for_buffer = np.asarray([ac[0] for ac in noisey_actions_for_buffer])
+#np.ndarray.flatten
+            actions_params_for_buffer = np.array([[np.concatenate((ac,pm),axis=0) for ac,pm in zip(noisey_actions_for_buffer,params)] for i in range(1)]).reshape(
+                env.num_TA,env.action_params.shape[1] + len(env.action_list)) # concatenated actions, params for buffer
+            agents_actions = [np.argmax(agent_act_one_hot) for agent_act_one_hot in noisey_actions_for_buffer] # convert the one hot encoded actions  to list indexes 
             obs =  np.array([env.Observation(i,'team') for i in range(maddpg.nagents)]).T
-
+            
 
             # If kickable is True one of the teammate agents has possession of the ball
             kickable = False
@@ -211,7 +210,7 @@ for ep_i in range(0, num_episodes):
                 for u_i in range(1):
                     for a_i in range(maddpg.nagents):
                         sample = replay_buffer.sample(batch_size,
-                                                      to_gpu=False,norm_rews=False)
+                                                      to_gpu=False,norm_rews=True)
                         #print('a_i ' , a_i )
                         maddpg.update(sample, a_i )
                     maddpg.update_all_targets()

maddpg-pytorch/algorithms/maddpg.py
@@ -102,9 +102,12 @@ class MADDPG(object):
                 all_trgt_acs = [onehot_from_logits(pi(nobs)) for pi, nobs in
                                 zip(self.target_policies, next_obs)]
             else:
-                all_trgt_acs = [pi(nobs) for pi, nobs in zip(self.target_policies,
-                                                             next_obs)]
+                all_trgt_acs = [torch.cat(
+                    (onehot_from_logits(pi(nobs)[:,:curr_agent.action_dim]),pi(nobs)[:,curr_agent.action_dim:]),1)
+                                for pi, nobs in zip(self.target_policies, next_obs)]    # onehot the action space but not param
+                
             trgt_vf_in = torch.cat((*next_obs, *all_trgt_acs), dim=1)
+                    
         else:  # DDPG
             if self.discrete_action:
                 trgt_vf_in = torch.cat((next_obs[agent_i],
@@ -175,7 +178,7 @@ class MADDPG(object):
 
         hook = vf_in.register_hook(self.inject)
         pol_loss = -curr_agent.critic(vf_in).mean()
-        pol_loss += (curr_pol_out**2).mean() * 1e-3 # regularize size of action
+        #pol_loss += (curr_pol_out[:curr_agent.action_dim]**2).mean() * 1e-2 # regularize size of action
         pol_loss.backward()
         hook.remove()
 

maddpg-pytorch/utils/networks.py
@@ -45,8 +45,10 @@ class MLPNetwork(nn.Module):
 
             self.out_param = nn.Linear(128, self.param_size)
             self.out_param.weight.data.normal_(0, 0.01) 
-            #self.out_param_fn = F.tanh
             self.out_param_fn = lambda x: x
+            self.out_action_fn = F.softmax
+            #self.out_action_fn = lambda x: x
+
         else:
             self.out = nn.Linear(128,out_dim)
             self.out.weight.data.normal_(0, 0.01) 
@@ -71,13 +73,13 @@ class MLPNetwork(nn.Module):
         
         if self.is_actor and not self.discrete_action:
  
-            self.final_out_action = self.out_fn(self.out_action(h4))
+            self.final_out_action = self.out_action_fn(self.out_action(h4))
             self.final_out_params = Variable(self.out_param_fn(self.out_param(h4)),requires_grad=True)
             out = torch.cat((self.final_out_action, self.final_out_params),1)
-            #if self.count % 100 < 1:
-                #print(out)
+            if self.count % 100 < 1:
+                print(out)
             self.count += 1
-           # print("OUT",out)
+            #print("OUT",out)
 
         else:
             out = self.out_fn(self.out(h4))