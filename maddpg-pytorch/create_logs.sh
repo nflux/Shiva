5573c2df17c6ede4145b25f672afc6e0112723a9
Daniel Tellier <telldanieljames@gmail.com>
2019-02-10 18:35:07 -0800

    Fixed bash script to run multiple create python scripts in parallel

--- maddpg-pytorch/create_logs.sh
+++ maddpg-pytorch/create_logs.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-NUM_ENVS=3
+NUM_ENVS=4
 py_command=""
 
 for ((i=1; i <= $NUM_ENVS; i++ ))
 do
     j=$((($i+1) * 1000))
-    if ["$j" != "$((($NUM_ENVS+1) * 1000))"]
-    then
-        $py_command="python create_pretrain_files.py --port $j --log_dir log_$j | ${py_command}"
+    if [ "$j" != "$((($NUM_ENVS+1) * 1000))" ]; then
+        temp="python create_pretrain_files.py --port $j --log_dir log_$j |"
+        py_command="$py_command $temp"
     else
-        $py_command="${py_command}python create_pretrain_files.py --port $j --log_dir log_$j"
+        temp="python create_pretrain_files.py --port $j --log_dir log_$j"
+        py_command="$py_command $temp"
     fi
 done
 
