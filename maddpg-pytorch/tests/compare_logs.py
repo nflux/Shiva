fa30a0883bae55f077b4818c542c6e3074f8999b
Daniel Tellier <telldanieljames@gmail.com>
2019-03-14 15:24:53 -0700

    comparing logs

---
+++ maddpg-pytorch/tests/compare_logs.py
@@ -0,0 +1,26 @@
+from os import path
+import numpy as np
+
+basepath = path.dirname(__file__)
+filepath = path.abspath(path.join(basepath, "..", "pt_logs_2000/log_obs_left_1.csv"))
+f = open(filepath, "r")
+
+file_list = []
+for i,line in enumerate(f):
+    if i == 0 or i == 1:
+        continue
+    file_list.append(line.split(','))
+
+set_of_indices = set()
+file_np = np.asarray(file_list)
+for f in range(0,len(file_np)-1,2):
+    print(f, end=' ')
+    for i in range(len(file_np[f])):
+        if abs(float(file_np[f,i]) - float(file_np[f+1,i])) > 0.1e-5:
+            print(i-1, end=' ')
+            set_of_indices.add(i-1)
+    print('\n--------------')
+    # if f == 10:
+    #     break
+
+print(set_of_indices)
