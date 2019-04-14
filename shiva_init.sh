4cbd80a5618936eefb08330c524171f2aec71d5f
Daniel Tellier <telldanieljames@gmail.com>
2019-04-14 07:22:52 -0700

    add file to init repos

---
+++ shiva_init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Description: This file is meant to setup a local environment for modification of HFO
+#Note: Change checkout branches to customize configuration.
+
+mkdir shiva
+cd shiva
+
+#Get the RCSSSERVER Repo
+git clone https://github.com/mehrzadshabez/rcssserver.git
+cd rcssserver
+git checkout gen-pt
+cd ..
+
+#Get the LIBRCSC Repo
+git clone https://github.com/mehrzadshabez/librcsc.git
+cd librcsc
+git checkout dev
+cd ..
+
+
+#Get the Robocup-Sigma Repo
+git clone https://github.com/mehrzadshabez/Robocup-Sigma.git
+cd Robocup-Sigma
+git checkout dev
+
+cd maddpg-pytorch/HFO
+bash recompile_start.sh
+
+#Get Matthews Soccerwindow (Proabably unnecessary)
+#git clone https://github.com/mhauskn/soccerwindow2.git
+
+#Get the Matthew hfo-teams (Proabably unnecessary)
+#git clone https://github.com/mhauskn/hfo-teams.git
