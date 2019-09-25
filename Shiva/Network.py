b41a66df0907d5de1fc85044296ded8b85eab235
nflux-dtellier <dtellier@nflux.ai>
2019-09-24 21:17:02 -0700

    Brought DQNet into Network.py

--- Shiva/Network.py
+++ Shiva/Network.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 class Network(torch.nn.Module):
     def __init__(self, input_shape, output_shape):
@@ -7,3 +8,31 @@ class Network(torch.nn.Module):
     
     def forward():
         pass
+
+class DQNet(Network):
+    def __init__(self, input_shape, n_actions):
+        super(DQNet, self).__init__(input_shape, n_actions)
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(self.input_shape)
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, self.output_shape)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        return self.fc(conv_out)
