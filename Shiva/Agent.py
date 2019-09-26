e242e2a42bb8740df1cf9d2978ef6f133f0c97ec
Joshua Kristanto <jkristanto@nflux.ai>
2019-09-25 21:33:09 -0700

    Modified Init of Agent, and Started on Policy

--- Shiva/Agent.py
+++ Shiva/Agent.py
@@ -1,16 +1,69 @@
+import numpy as np
+import uuid
 class Agent:
     def __init__(self, obs_dim, action_dim, uid):
         self.obs_dim = obs_dim
         self.action_dim = action_dim
-        self.id = uid # unique id
+        self.id = uuid.uuid4()
+        self.network = Network(self.obs_dim,self.action_dim)
         self.policy = None
         self.target_policy = None
+       
     
     def policy(self, obs):
-        pass
+        rand = np.random.random()
+        tensor = self.network.Forward(obs)
+        if np.random.ramdon() < 1 - self.epsilon:
+
+        else:
+            
+      
+        '''
+            Obtain Action from current policy. 
+        '''
+        return action
 
     def save(self):
+         '''
+            Save the current Obs and Action to obs_
+        '''
         pass
 
     def load(self):
+        '''
+        Load the Cunrrent action load the Replay Buffer
+        '''
+
+        pass
+
+
+
+class DQAgent:
+    def __init__(self, obs_dim, action_dim, uid):
+        self.obs_dim = obs_dim
+        self.action_dim = action_dim
+        self.id = uuid.uuid4()
+        self.network = Network(self.obs_dim,self.action_dim)
+        self.policy = None
+        self.target_policy = None
+       
+    
+    def policy(self, obs):
+        action = self.network.Forward(obs)
+        '''
+            Obtain Action from current policy. 
+        '''
+        return action
+
+    def save(self):
+         '''
+            Save the current Obs and Action
+        '''
+        pass
+
+    def load(self):
+        '''
+        Load the Cunrrent action
+        '''
+
         pass
