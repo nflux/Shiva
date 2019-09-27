58449837b9c19c84d3e6ba03a073d4fe645111e4
nflux-jmartinez <55065710+nflux-jmartinez@users.noreply.github.com>
2019-09-26 20:07:08 -0700

    Update Network.py

--- Shiva/Network.py
+++ Shiva/Network.py
@@ -10,29 +10,16 @@ class Network(torch.nn.Module):
         pass
 
 class DQNet(Network):
-    def __init__(self, input_shape, n_actions):
-        super(DQNet, self).__init__(input_shape, n_actions)
-
-        self.conv = nn.Sequential(
-            nn.Conv2d(self.input_shape[0], 32, kernel_size=8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1),
-            nn.ReLU()
-        )
-
-        conv_out_size = self._get_conv_out(self.input_shape)
-        self.fc = nn.Sequential(
-            nn.Linear(conv_out_size, 512),
-            nn.ReLU(),
-            nn.Linear(512, self.output_shape)
-        )
-
-    def _get_conv_out(self, shape):
-        o = self.conv(torch.zeros(1, *shape))
-        return int(np.prod(o.size()))
-
-    def forward(self, x):
-        conv_out = self.conv(x).view(x.size()[0], -1)
-        return self.fc(conv_out)
+    
+    def __init__(self, input_size, hidden_layer1, hidden_layer2, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_layer1)
+        self.linear2 = nn.Linear(hidden_layer1, hidden_layer2)
+        self.linear3 = nn.Linear(hidden_layer2, output_size)
+        
+    def forward(self,x):
+        x = torch.relu(self.linear1(x))
+        x = torch.relu(self.linear2(x))
+        x = torch.relu(self.linear3(x))
+        return x
+   
