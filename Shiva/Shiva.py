4eedb5171a61d419ea60f8037ce9cfc111adcda0
Ez <edonovan@nflux.ai>
2019-10-15 20:20:23 -0700

    added documentation to ShivaAdmin

--- Shiva/Shiva.py
+++ Shiva/Shiva.py
@@ -7,52 +7,53 @@ from tensorboardX import SummaryWriter
 ###################################################################################
 # 
 #   ShivaAdmin
-#       Input
-#            @INIT_DIR      Absolute address location for initialization file
+#       This administrator class is a filing helper for the Shiva framework
+#
+#       Part of it's tasks are:
+#       
+#           - Track and create directories for the MetaLearners, Learners, Agents
+#           - Perform the mechanics of saving and loading of MetaLearners, Learners, Agents which include
+#                   - The config files used
+#                   - The Agent class pickle
+#                   - The Agent networks (implemented at the Agent level due to the different Agents specifications)
+#           - Add a SummaryWriter for the Agents and the metrics for a given agent
+#
+#       The folders structure being used are given by the input @init_dir on ShivaAdmin instantiation
+#       Requirements of the .INI file
+#
+#            [SHIVA]
+#            save =              True           Saving option
+#            traceback =         True           Debugging traceback option
+#
+#            [DIRECTORY]
+#            runs =              "/runs"        Folder where the runs will be saved
+#            inits =             "/inits"       Config files that will be passed to the MetaLearner
+#            modules =           "/modules"     Folder where the MetaLearner, Learner, Agents, Environment, etc..
+#            utils =             "/utils"       Utilities/helpers folder
+#            data =              "/data"        Some other data?
+#
 #         
 ###################################################################################
 
 class ShivaAdmin():
-    '''
-        This object will take care of all the saving/loading of the modules
-        Will strictly depend on the configs provided by the INIT_INI file
-
-        Attributes
-            @mode
-                Possible modes:
-                    productions     create new agents per the configs
-                    evaluation      uses the agents in file and evaluate them per the configs
-
-            @dirs
-                Provided under the INIT_INI[DIRECTORY] section
-                Used to provide the absolute directories for the saves/loads
-
-        Public Methods
-            @save(caller)
-                Identifies who is the caller and execute the appropiate method
-                g.e. identify if the caller is either a MetaLearner or a Learner
-            
-            @load(caller)
-                Same as above, but loads per the config
-    '''
     def __init__(self, init_dir):
+        '''
+            Input
+                @init_dir       Absolute path for the ShivaAdmin ini file declared in settings.py
+        '''
         self._curr_meta_learner_dir = ''
         self._curr_learner_dir = {}
         self._curr_agent_dir = {}
         self.writer = {}
         
-        self.MODE_PRODUCTION = 'production'
-        self.MODE_EVALUATION = 'evaluation'
-        self.VALID_MODES = [self.MODE_PRODUCTION, self.MODE_EVALUATION]
         self._INITS = helpers.load_config_file_2_dict(init_dir)
         
         assert 'SHIVA' in self._INITS, 'Needs a [SHIVA] section in file {}'.format(init_dir)
-        assert 'mode' in self._INITS['SHIVA'] and self._INITS['SHIVA']['mode'] in self.VALID_MODES, 'Need a valid start mode in [SHIVA] section in file {}'.format(init_dir)
         assert 'DIRECTORY' in self._INITS, 'Need [DIRECTORY] section in file {}'.format(init_dir)
-        self.mode = self._INITS['SHIVA']['mode']
+        
         self.need_to_save = self._INITS['SHIVA']['save']
         self.dirs = self._INITS['DIRECTORY']
-        self._parse_dirs()
+        self._set_dirs_attrs()
 
         if self._INITS['SHIVA']['traceback']:
             helpers.warnings.showwarning = helpers.warn_with_traceback
@@ -60,67 +61,65 @@ class ShivaAdmin():
     def __str__(self):
         return "ShivaAdmin"
     
-    def _parse_dirs(self):
+    def _set_dirs_attrs(self) -> None:
         '''
-            Set self attributes for the DIRECTORY given in INIT.init
-            As well as create the folders if they don't exist
+            Set self attributes for accessing directories
+            These directories come from the section DIRECTORY in the init.ini (or other given by user at settings.py
+            The procedure as well creates the needed folders for this directories if they don't exist
         '''
-        assert (self.mode == 'production' and 'inits' in self.dirs) or (self.mode == 'evaluation' and 'data' in self.dirs), "For Shiva mode {} we are missing either 'inits' for production or 'data' for evaluation".format(self.mode)
         self.base_url = os.path.abspath('.')
         for key, folder_name in self.dirs.items():
             directory = self.base_url + folder_name
             setattr(self, key+'_url', directory)
             helpers.make_dir(os.path.join(self.base_url, directory))
 
-    ###
-    #    Formats the config file for the MetaLearner
-    ###
-
-    def get_inits(self):
+    def get_inits(self) -> list:
         '''
-            This method came from the _old/Validation.py
+            This procedure reads the *.ini files in path given by user at the init.ini file, section [DIRECTORY], attribute INITS
 
-            This method will assume that we are going to read more than one config file or a folders of config files in the INITS folder
-            If its a single config file then it will be assumed that all the configurations for the learner will be in that file
-            If its a folder of config files it will expect there to be a config file for each component
-            I can design this so that read configs will go through each file and identify what kind of learner method needs to be implemented
+            Returns
+                A list of config dictionaries
         '''
-        
-        if self.mode == self.MODE_PRODUCTION:
-            self.meta_learner_config = helpers.parse_configs(self.inits_url)
-            return self.meta_learner_config
-        elif self.mode == self.MODE_EVALUATION:
-            '''
-                Check if instances to load are there
-                Read them and return
-            '''
-            self.init_loader = helpers.configparser(self.inits_url)
-            meta_learner_to_load = self.init_loader[0]['MetaLearner']['load_run'] + self.output_config_name
-            if os.path.isfile(os.path.join(self.runs_url, meta_learner_to_load)):
-                print('MetaLearner file requested found at {}'.format(meta_learner_to_load))
-
-    ###
-    #   Administration of directory folders
-    ###
+        self.meta_learner_config = helpers.parse_configs(self.inits_url)
+        return self.meta_learner_config
     
-    def add_meta_profile(self, meta_learner, env_name: str=''):
+    def add_meta_profile(self, meta_learner, env_name: str='') -> None:
+        '''
+            This method would be called by a Meta Learner in order to add himself
+            Is needed in order to keep track of the Meta Learner directory.
+            Creates a folder for it if save flag is True.
+
+            Input
+                @meta_learner       Meta Learner instance to be filed
+                @env_name           Environment name to append to the Meta Learner folder
+        '''
         if self.need_to_save:
             ml_folder_name = 'ML-'+env_name+'-'
             self._curr_meta_learner_dir = helpers.make_dir_timestamp(os.path.join(self.base_url, self.runs_url, ml_folder_name))
 
-    def add_learner_profile(self, learner):
-        # print('add_learner_profile', learner)
+    def add_learner_profile(self, learner) -> None:
+        '''
+            Needed to keep track of the Learner and it's directory.
+            Creates a folder for it if save flag is True.
+
+            Input
+                @learner            Learner instance to be filed
+        '''
         if not self.need_to_save: return
         if learner.id not in self._curr_learner_dir:
             self._curr_learner_dir[learner.id] = helpers.make_dir(os.path.join(self._curr_meta_learner_dir, 'L-'+str(learner.id)))
             self._curr_agent_dir[learner.id] = {}
             self.writer[learner.id] = {}
-            # print('Learner', learner.id, 'profile added')
 
     def update_agents_profile(self, learner):
-        # print('update_agents_profile', learner)
+        '''
+            This procedure adds all the agents profile inside the learner
+
+            Input
+                @learner            Learner instance
+        '''
         if not self.need_to_save: return
-        self.add_learner_profile(learner)
+        self.add_learner_profile(learner) # will only add if was not profiled before
         if type(learner.agents) == list:
             for agent in learner.agents:
                 self._add_agent_profile(learner, agent)
@@ -128,34 +127,66 @@ class ShivaAdmin():
             self._add_agent_profile(learner, learner.agents)
 
     def _add_agent_profile(self, learner, agent):
+        '''
+            Needed to keep track of the agents and it's directory.
+            Creates a folder for it if save flag is True.
+
+            Input
+                @learner            Learner instance owner of the Agent
+                @agent              Agent instance to be filed
+        '''
         if not self.need_to_save: return
         if agent.id not in self._curr_agent_dir[learner.id]:
             self._curr_agent_dir[learner.id][agent.id] = helpers.make_dir(os.path.join(self._curr_learner_dir[learner.id], 'Agents', str(agent.id)))
             self.init_summary_writer(learner, agent)
-            # print('Agent', agent.id, 'profile added')
 
-    def get_agent_dir(self, learner, agent):
-        # print('get_agent_dir', learner, agent)
+    def get_agent_dir(self, learner, agent) -> str:
+        '''
+            Returns the absolute address location of the given agent
+            
+            Input
+                @learner            Learner instance owner of the Agent
+                @agent              Agent instance
+        '''
         self._add_agent_profile(learner, agent)
         return self._curr_agent_dir[learner.id][agent.id]
 
-    ###
-    # Summary Writer Utilities
-    ###
+    def init_summary_writer(self, learner, agent) -> None:
+        '''
+            Instantiates the SummaryWriter for the given agent
 
-    def init_summary_writer(self, learner, agent):
+            Input
+                @learner            Learner instance owner of the Agent
+                @agent              Agent who we want to records the metrics
+        '''
         if not self.need_to_save: return
         self.writer[learner.id][agent.id] =  SummaryWriter(self.get_agent_dir(learner, agent))
 
-    def add_summary_writer(self, learner, agent, scalar_name, value_y, value_x):
+    def add_summary_writer(self, learner, agent, scalar_name, value_y, value_x) -> None:
+        '''
+            Adds a metric to the tensorboard of the given agent
+
+            Input
+                @learner            Learner instance owner of the agent
+                @agent              Agent who we want to add
+                @scalar_name        Metric name
+                @value_y            Usually the metric
+                @value_x            Usually time
+        '''
         if not self.need_to_save: return
         self.writer[learner.id][agent.id].add_scalar(scalar_name, value_y, value_x)
 
-    ###
-    # Saving Implementations
-    ###
+    def save(self, caller) -> None:
+        '''
+            This procedure is for the MetaLearner (or Learner) to save all it's configurations and agents
+            If a MetaLearner is the caller, the saving will cascade along all the Learner that it has, and all the agents inside the Learner
+            
+            Requirement
+                The caller, before saving, must have added his profile, if not, an error will be thrown
 
-    def save(self, caller):
+            Input
+                @caller             Either a MetaLearner or Learner instance
+        '''
         if not self.need_to_save: return
         self.caller = caller
         if 'metalearner' in inspect.getfile(self.caller.__class__).lower():
@@ -163,9 +194,16 @@ class ShivaAdmin():
         elif 'learner' in inspect.getfile(self.caller.__class__).lower():
             self._save_learner()
         else:
-            assert False, "{} couldn't identify who is trying to save. Only valid for a MetaLearner or Learner (sub)classes. Got {}".format(self, self.caller)
+            assert False, "{} couldn't identify who is trying to save. Only valid for a MetaLearner or Learner subclasses. Got {}".format(self, self.caller)
 
-    def _save_meta_learner(self):
+    def _save_meta_learner(self) -> None:
+        '''
+            Mechanics of saving a MetaLearner
+                1-  Saves each of the config files used
+                2-  Then iterates over all it's Learners to save them
+
+            No input because we are only handling only ONE MetaLearner
+        '''
         print("Saving Meta Learner:", self.caller, '@', self._curr_meta_learner_dir)
         # create the meta learner configs folder
         helpers.make_dir(os.path.join(self._curr_meta_learner_dir, 'configs'))
@@ -177,9 +215,16 @@ class ShivaAdmin():
         for learner in self.caller.learners:
             self._save_learner(learner)
 
-    def _save_learner(self, learner=None):
+    def _save_learner(self, learner=None) -> None:
+        '''
+            Mechanics of saving a Learner
+                Will cascade along all it's agents
+
+            Input
+                @learner        Learner instance we want to save
+        '''
         learner = self.caller if learner is None else learner
-        self.add_learner_profile(learner)
+        self.add_learner_profile(learner) # will only add if was not profiled before
         if type(learner.agents) == list:
             for agent in learner.agents:
                 self._save_agent(learner, agent)
@@ -187,17 +232,28 @@ class ShivaAdmin():
             self._save_agent(learner, learner.agents)
 
     def _save_agent(self, learner, agent):
+        '''
+            Mechanics of saving a Agent
+                1-  Pickles the agent object
+                2-  Uses the agent.save() method which they save their networks (is implemented at the Agent level because they might have multiple networks)
+
+            Input
+                @learner        Learner who owns the agent
+                @agent          Agent we want to save
+        '''
         self._add_agent_profile(learner, agent)
         agent_url = self._curr_agent_dir[learner.id][agent.id]
         agent.save(agent_url, learner.env.get_current_step())
         with open(agent_url+'/cls.pickle', 'wb') as handle:
             pickle.dump(agent, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    ###
-    # Loading Implementations
-    ###
-
     def load(self, caller, id=None):
+        '''
+            TODO
+                I don't see a point of having this function for now
+                I guess will have the specific implementations as below
+                To think about..
+        '''
         self.caller = caller
         self.id = id
         if 'metalearner' in inspect.getfile(caller.__class__).lower():
@@ -208,12 +264,33 @@ class ShivaAdmin():
             assert False, '{} loading error'.format(self)
 
     def _load_meta_learner(self):
+        '''
+            TODO
+                Implement once needed in order to see what's the best approach
+                
+            Returns
+                MetaLearner instance
+        '''
         print("Loading MetaLearner")
 
     def _load_learner(self):
+        '''
+            TODO
+                Implement once needed in order to see what's the best approach
+
+            Returns
+                Learner instance
+        '''
         print("Loading Learner")
 
-    def _load_agents(self, path):
+    def _load_agents(self, path) -> list:
+        '''
+            For a given @path, the procedure will walk recursively over all the folders inside the @path
+            And find all the .pickle and .pth files to load all those agents
+
+            Input
+                @path       Path where the agents files will be located
+        '''
         agents = []
         agents_pickles = helpers.find_pattern_in_path(path, '*.pickle')
         agents_policies = helpers.find_pattern_in_path(path, '*.pth')
