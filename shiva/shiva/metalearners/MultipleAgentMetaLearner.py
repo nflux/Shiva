4f9e204f3495ad47f6b5cde64207ba3a1bf90085
Travis Hamm <thamm@nflux.ai>
2019-11-14 18:38:16 -0800

    Saves population learners after separate processes are run

--- shiva/shiva/metalearners/MultipleAgentMetaLearner.py
+++ shiva/shiva/metalearners/MultipleAgentMetaLearner.py
@@ -9,15 +9,18 @@ import learners
 import torch
 import random
 
+
 class MultipleAgentMetaLearner(MetaLearner):
     def __init__(self, configs):
         super(MultipleAgentMetaLearner,self).__init__(configs)
         self.configs = configs
         self.learnerCount = 0
-        self.learnerList = configs["MetaLearner"]["learner_list"]
+        self.learner_list_size = configs["MetaLearner"]["learner_list"]
+        self.learners = [None] * configs['MetaLearner']['learner_list']
         self.learning_rate_range = configs["MetaLearner"]["learning_rate"]
         self.process_list = list()
-        self.multiprocessing_learners()
+        #self.multiprocessing_learners()
+        self.run()
 
     def run(self):
 
@@ -36,10 +39,10 @@ class MultipleAgentMetaLearner(MetaLearner):
 
         elif self.start_mode == self.PROD_MODE:
 
-            # agents, environments, algorithm, data, configs for a single agent learner
+            '''# agents, environments, algorithm, data, configs for a single agent learner
             #agents, environments, algorithm, data, config
             self.learner = self.create_learner()
-            
+
             # self.learner = self.create_learner(self.agent, self.eval_env, self.algorithm, self.buffer, self.learner_config)
             shiva.add_learner_profile(self.learner)
 
@@ -48,7 +51,12 @@ class MultipleAgentMetaLearner(MetaLearner):
             shiva.update_agents_profile(self.learner)
 
             # Runs the learner for a number of episodes given by the config
-            self.learner.run()
+            self.learner.run()'''
+
+            self.populate_learners()
+
+
+            self.multiprocessing_learners()
 
             # save
             self.save()
@@ -60,12 +68,33 @@ class MultipleAgentMetaLearner(MetaLearner):
         self.configs['Agent']['learning_rate'] = random.uniform(self.learning_rate_range[0],self.learning_rate_range[1])
         print (self.configs['Agent']['learning_rate'])
         return learner(self.get_id(), self.configs)
-    
-    #threading learners
+
+    '''#threading learners
     def multiprocessing_learners(self):
         for rank in range(self.learnerList):
             p = torch.multiprocessing.Process(target=self.run)
             p.start()
             self.process_list.append(p)
         for p in self.process_list:
+            p.join()'''
+
+    def multiprocessing_learners(self):
+        for learner in range(len(self.learners)):
+            p = torch.multiprocessing.Process(target=self.run_learner(learner))
+            p.start()
+            self.process_list.append(p)
+        for p in self.process_list:
             p.join()
+
+
+    def run_learner(self,learner_idx):
+        shiva.add_learner_profile(self.learners[learner_idx])
+        self.learners[learner_idx].launch()
+        shiva.update_agents_profile(self.learners[learner_idx])
+        self.learners[learner_idx].run()
+
+
+    #fill the list of learners
+    def populate_learners(self):
+        for learner in range(self.learner_list_size):
+            self.learners[learner] = self.create_learner()
