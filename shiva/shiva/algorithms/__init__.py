4a47551bde0d27bf2f23bb8810cb02ec1b763417
Ezequiel <edonovan@nflux.ai>
2019-11-24 16:10:52 -0800

    merge

--- shiva/shiva/algorithms/__init__.py
+++ shiva/shiva/algorithms/__init__.py
@@ -2,3 +2,4 @@ from algorithms.DQNAlgorithm import *
 from algorithms.ContinuousDDPGAlgorithm import *
 from algorithms.DaggerAlgorithm import *
 from algorithms.SupervisedAlgorithm import *
+from algorithms.DiscreteDDPGAlgorithm import *
