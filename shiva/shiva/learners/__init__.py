69e582c1d3ab8f4ed0649c295a6d875fadebd92d
Ezequiel <edonovan@nflux.ai>
2019-11-28 03:40:35 -0800

    unity python hookup

--- shiva/shiva/learners/__init__.py
+++ shiva/shiva/learners/__init__.py
@@ -2,3 +2,4 @@ from learners.SingleAgentDDPGLearner import *
 from learners.SingleAgentDQNLearner import *
 from learners.SingleAgentImitationLearner import *
 from learners.DDPGLearner import *
+from learners.UnityLearner import *
