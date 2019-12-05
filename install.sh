308d915f9eeb93ef19b562d818f4e074561b318c
Jorge <jmartinez@nflux.ai>
2019-12-05 00:20:55 -0800

    Last change to installation script

--- install.sh
+++ install.sh
@@ -7,21 +7,49 @@
 ## Installing python and venv, and creating a virtual environment
 
 # Installs python 3
-sudo apt install python3
+sudo apt install python3-pip
 # Installs venv
 sudo apt-get install python3-venv
 cd ..
 # creates a virtual environment just for shiva
-python3 -m venv shiva
+python3 -m venv Shiva
 # activates the virtual environment
 source shiva/bin/activate
+# ​Go to robocup folder
+cd Control-Tasks
 
-## Install Unity API 
+cd shiva/shiva/envs/robocup/
 
-cd Control-Tasks/shiva/shiva/envs/ml-agents/ml-agents-envs/
+install git
+sudo apt-get install git
+
+# clone the robocup files
+git clone https://github.com/nflux/rcssserver.git
+git clone https://github.com/nflux/librcsc.git
+
+# Install cmake, C++ Boost Libraries, Flex and Qt version 4
+sudo apt-get install cmake libboost-dev libboost-all-dev flex qt4-default
+cd HFO
+
+#The above command will create a build/ folder with various executable files
+bash compile.sh
+​
+## Ready to launch a RoboCup session
+
+# cd shiva/shiva/envs/ml-agents/ml-agents-envs/
+
+# Install Unity Python API
+cd ../../ml-agents/ml-agents-envs/
 pip install -e ./
-cd ../ml-agents
+cd ..
+cd ml-agents
 pip install -e ./
+
+## Unity API installed
+
+
+### Install cuda and video drivers here
+
 
 # Python dependencies
 pip install gym tensorboard tensorboardX tensorflow pandas torch pynput jupyter
