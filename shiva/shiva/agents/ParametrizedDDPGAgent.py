1b34681e53e045d1a6fc7af0b1e011450c40944c
nflux-dtellier <dtellier@nflux.ai>
2019-12-08 18:00:41 -0800

    removed pointless folder from main dir

--- shiva/shiva/agents/ParametrizedDDPGAgent.py
+++ shiva/shiva/agents/ParametrizedDDPGAgent.py
@@ -32,8 +32,8 @@ class ParametrizedDDPGAgent(Agent):
         # input()
         
     def find_best_imitation_action(self, observation: np.ndarray) -> np.ndarray:
-            observation = torch.tensor(observation).to(self.device)
-            action = self.actor(observation.float()).cpu().data.numpy()
-            action = np.clip(action, -1,1)
-            # print('actor action shape', action.shape)
-            return action[0]
+        observation = torch.tensor(observation).to(self.device)
+        action = self.actor(observation.float()).cpu().data.numpy()
+        action = np.clip(action, -1,1)
+        # print('actor action shape', action.shape)
+        return action[0]
