b0afc821dcf38d1ebbf19e0694fda617edb41d20
nflux-dtellier <dtellier@nflux.ai>
2019-12-08 16:42:47 -0800

    updated buffer

--- shiva/shiva/buffers/SimpleBuffer.py
+++ shiva/shiva/buffers/SimpleBuffer.py
@@ -8,7 +8,7 @@ class SimpleBuffer():
         Expected Configs:
 
             batch_size
-            capacity
+            capacity    
 
     '''
 
@@ -18,22 +18,50 @@ class SimpleBuffer():
         self.buffer = collections.deque(maxlen=self.capacity)
         self.batch_size = batch_size
 
+
     def __len__(self):
         return len(self.buffer)
 
     def append(self, experience):
         self.buffer.append(experience)
-        
+
     def clear_buffer(self):
         self.buffer = collections.deque(maxlen=self.capacity)
 
     def sample(self):
         indices = np.random.choice(len(self.buffer), self.batch_size)
-        states, actions, rewards,next_states, dones = zip(*[self.buffer[idx] for idx in indices])
-        return np.array(states), np.array(actions), np.array(rewards, dtype=np.float32),\
+        states, actions, rewards, next_states, dones = zip(*[self.buffer[idx] for idx in indices])
+        return np.array(states), np.array(actions), np.array(rewards, dtype=np.float32), \
                np.array(next_states), np.array(dones, dtype=np.bool)
 
-    def full_buffer(self):
-        states,actions,rewards,next_states,dones = zip(*[self.buffer[idx] for idx in range(len(self.buffer))])
+class SimpleRoboCupBuffer():
+
+    '''
+        Expected Configs:
+
+            batch_size
+            capacity    
+
+    '''
+
+    def __init__(self, batch_size, capacity):
+        # super(SimpleBuffer, self).__init__(max_size, num_agents, obs_dim, acs_dim)
+        self.capacity = capacity
+        self.buffer = collections.deque(maxlen=self.capacity)
+        self.batch_size = batch_size
+
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def append(self, experience):
+        self.buffer.append(experience)
+
+    def clear_buffer(self):
+        self.buffer = collections.deque(maxlen=self.capacity)
+
+    def sample(self):
+        indices = np.random.choice(len(self.buffer), self.batch_size)
+        states, actions, rewards, next_states, dones, bot_actions = zip(*[self.buffer[idx] for idx in indices])
         return np.array(states), np.array(actions), np.array(rewards, dtype=np.float32), \
-             np.array(next_states), np.array(dones, dtype=np.bool)
+               np.array(next_states), np.array(dones, dtype=np.bool), np.array(bot_actions)
