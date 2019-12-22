0c7c422e0a4b27d0974fb8b726ccfa2d3962990c
Jorge <jmartinez@nflux.ai>
2019-12-21 21:22:09 -0800

    Changed file names, about to begin discretizing robocup

--- shiva/shiva/agents/DDPGAgent.py
+++ shiva/shiva/agents/DDPGAgent.py
@@ -1,42 +1,33 @@
-import copy
-import torch
 import numpy as np
+import torch
+import copy
 
 from shiva.agents.Agent import Agent
-from shiva.networks.DDPGActor import DDPGActor
-from shiva.networks.DDPGCritic import DDPGCritic
-from shiva.helpers import misc
+from shiva.networks.DynamicLinearNetwork import DynamicLinearNetwork, SoftMaxHeadDynamicLinearNetwork
 
-from networks.DynamicLinearNetwork import SoftMaxHeadDynamicLinearNetwork
+class ParametrizedDDPGAgent(Agent):
+    def __init__(self, id, obs_dim, action_dim, param_ix, agent_config: dict, networks: dict):
+        super(ParametrizedDDPGAgent, self).__init__(id, obs_dim, action_dim, agent_config, networks)
+        try:
+            torch.manual_seed(self.manual_seed)
+            np.random.seed(self.manual_seed)
+        except:
+            torch.manual_seed(5)
+            np.random.seed(5)
 
-
-class DDPGAgent(Agent):
-    def __init__(self, id, obs_dim, action_dim, agent_config: dict, networks: dict):
-        super(DDPGAgent, self).__init__(id, obs_dim, action_dim, agent_config, networks)
         self.id = id
 
-        # print("Look here: ", networks)
-
-        # print("DDPG Agent:", obs_dim, action_dim)
-
-
-        self.actor = SoftMaxHeadDynamicLinearNetwork(obs_dim,
-                                    action_dim,
-                                    action_dim,
-                                    networks['actor'])
-
+        self.actor = SoftMaxHeadDynamicLinearNetwork(obs_dim, action_dim, param_ix, networks['actor'])
         self.target_actor = copy.deepcopy(self.actor)
 
-        self.critic = DDPGCritic(obs_dim,
-                                        action_dim,
-                                        networks['critic_head'],
-                                        networks['critic_tail'])
-
+        self.critic = DynamicLinearNetwork(obs_dim + action_dim, 1, networks['critic'])
         self.target_critic = copy.deepcopy(self.critic)
 
         self.actor_optimizer = self.optimizer_function(params=self.actor.parameters(), lr=self.learning_rate)
         self.critic_optimizer = self.optimizer_function(params=self.critic.parameters(), lr=self.learning_rate)
 
+    def get_action(self, observation):
+        return self.actor(observation)
 
         # Uncomment to check the networks
 
@@ -45,12 +36,10 @@ class DDPGAgent(Agent):
         # print(self.critic)
 
         # input()
-
-
+        
     def find_best_imitation_action(self, observation: np.ndarray) -> np.ndarray:
-
         observation = torch.tensor(observation).to(self.device)
         action = self.actor(observation.float()).cpu().data.numpy()
         action = np.clip(action, -1,1)
-
-        return action
+        # print('actor action shape', action.shape)
+        return action[0]
