e869ac51b46e9a3e3bcd1526bb2251b9f893532d
Joshua Kristanto <jkristanto@nflux.ai>
2019-12-29 17:03:25 -0800

    Moved some Scripts from Unity2

---
+++ shiva/shiva/envs/ml-agents/UnitySDK/Assets/ML-Agents/Scripts/ShivaScripts/OverideBehaviorControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class OverideBehaviorControl : MonoBehaviour
+{
+    [SerializeField]
+    BehaviorParameters [] param;
+    [SerializeField]
+    Agent []agent;
+    public void Update()
+    {
+        if (Input.GetKey("c"))
+        {
+            for (int i = 0; i < param.Length; i++)
+            {
+                if( param[i] != null)
+                {
+                    if (param[i].Heuristic == false)
+                        param[i].Heuristic = true;
+                    else if (param[i].Heuristic == true)
+                        param[i].Heuristic = false;
+                }
+
+
+            }
+           
+        }
+
+        if (Input.GetKey("d"))
+        {            
+           for ( int i = 0; i < param.Length; i ++)
+            {
+                if(agent[i] != null)
+                {
+                    if (agent[i].agentParameters.onDemandDecision == false)
+                        agent[i].agentParameters.onDemandDecision = true;
+                    else if (agent[i].agentParameters.onDemandDecision == true)
+                        agent[i].agentParameters.onDemandDecision = false;
+                }
+                
+            }
+            
+        }
+    }
+} 
